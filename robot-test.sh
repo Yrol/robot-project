c602c5a5d3a4be27c14b866aa79dc117dbcf7882
Yrol <yrollgayanth@yahoo.com>
2020-11-04 20:40:36 +1100

    Adding the basic classes

--- robot-test.sh
+++ robot-test.sh
@@ -3,8 +3,4 @@ path=/public/index
 
 if [ "$1" == "test" ]; then
   docker exec -it php bash -c "php application test"
-elif [ "$1" == "simulate-robot" ] && [ -n "$2" ]; then
-  docker exec php-cli bash -c "php application simulate-robot $2"
-elif [ "$1" == "simulate-robot" ]; then
-  docker exec php-cli bash -c "php application simulate-robot"
 fi
